6ca34a64cce3043225147e47ee72385e198c6a62
AraneaGT <araneaguatemala@gmail.com>
2024-05-11 23:34:34 -0600

    Add files via upload

--- js/backgroudcolor.js
+++
@@ -1,3 +0,0 @@
-const colorPrincipal = window.getComputedStyle(document.querySelector('.colorPrincipal')).color;
-document.documentElement.style.setProperty('--color-principal', colorPrincipal);
-document.querySelector('meta[name="theme-color"]').setAttribute('content', colorPrincipal);
